d137a18145948aec6c4a146b3286b1fcaa6dd5be
Joseph Andrade Viteri <desarrollowebandvit@gmail.com>
2023-07-20 23:30:44 +0200

    Configuración de la máquina virtual

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,63 +19,71 @@ resource "azurerm_resource_group" "rg" {
   location = var.location_name
 }
 
-# resource "azurerm_virtual_network" "vnet" {
-#   name                = var.network_name
-#   address_space       = ["10.0.0.0/16"]
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-# }
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.network_name
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
 
-# resource "azurerm_subnet" "subnet" {
-#   name                 = var.subnet_name
-#   resource_group_name  = azurerm_resource_group.rg.name
-#   virtual_network_name = azurerm_virtual_network.vnet.name
-#   address_prefixes     = ["10.0.2.0/24"]
-# }
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
 
-# resource "azurerm_network_interface" "nic" {
-#   name                = "vnic"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_network_interface" "nic" {
+  name                = "vnic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
-#   ip_configuration {
-#     name                          = "internal"
-#     subnet_id                     = azurerm_subnet.subnet.id
-#     private_ip_address_allocation = "Dynamic"
-#   }
-# }
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
 
-# resource "azurerm_linux_virtual_machine" "vm" {
-#   name                = "vm1"
-#   resource_group_name = azurerm_resource_group.rg.name
-#   location            = azurerm_resource_group.rg.location
-#   size                = "Standard_F2"
-#   admin_username      = "azureuser"
-#   network_interface_ids = [
-#     azurerm_network_interface.nic.id,
-#   ]
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "vmcaspractico2"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_F2"
+  admin_username      = "azureuser"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
 
-#   admin_ssh_key {
-#     username   = "azureuser"
-#     public_key = file("~/.ssh/id_rsa.pub")
-#   }
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("./.ssh/vm_azure.pub")
+  }
 
-#   os_disk {
-#     caching              = "ReadWrite"
-#     storage_account_type = "Standard_LRS"
-#   }
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
 
-#   plan {
-#     name      = "centos-8-stream-free"
-#     product   = "centos-8-stream-free"
-#     publisher = "cognosys"
-#   }
+  plan {
+    name      = "centos-8-stream-free"
+    product   = "centos-8-stream-free"
+    publisher = "cognosys"
+  }
 
 
-#   source_image_reference {
-#     publisher = "cognosys"
-#     offer     = "centos-8-stream-free"
-#     sku       = "centos-8-stream-free"
-#     version   = "22.03.28"
-#   }
-# }
+  source_image_reference {
+    publisher = "cognosys"
+    offer     = "centos-8-stream-free"
+    sku       = "centos-8-stream-free"
+    version   = "22.03.28"
+  }
+
+  depends_on = [ azurerm_marketplace_agreement.vm_azure ]
+}
+
+resource "azurerm_marketplace_agreement" "vm_azure" {
+  publisher = "cognosys"
+  offer     = "centos-8-stream-free"
+  plan      = "centos-8-stream-free"
+}
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,5 +1,5 @@
 variable "resource_group_name" {
-  default = "rg-createdbyTF"
+  default = "rg-caso-pracito-2"
 }
 
 variable "location_name" {
