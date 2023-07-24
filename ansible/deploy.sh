2f8b7d448fbb5750ee4f7132d7a381b524fc17d0
Joseph Andrade Viteri <desarrollowebandvit@gmail.com>
2023-07-24 21:09:38 +0200

    Ficheros del AKS

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,2 +1,5 @@
-# Desplegar la aplicación
+# Desplegar la aplicación en VM azure
 ansible-playbook -i hosts playbook.yml
+
+# Despliege de aplicación en AKS
+ansible-playbook playbook-aks.yaml
--- terraform/recursos.tf
+++ terraform/recursos.tf
@@ -168,3 +168,10 @@ resource "azurerm_kubernetes_cluster" "aks" {
     type = "SystemAssigned"
   }
 }
+
+resource "azurerm_role_assignment" "enablePulling" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
